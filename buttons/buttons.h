10a8a2db04cc488202533218aaa6778262a607b7
momefilo <martin.mamon.pfalz@gmail.com>
2024-02-16 09:49:29 +0100

    add buttons

---
+++ buttons/buttons.h
@@ -0,0 +1,17 @@
+// momefilo Desing
+#ifndef momefilo_buttons_h
+#define momefilo_buttons_h 1
+
+#include "pico/stdlib.h"
+
+#define BUTTON_U 6
+#define BUTTON_D 7
+#define BUTTON_L 8
+#define BUTTON_R 9
+
+/* Initialisiert gpio */
+void buttons_init();
+
+/* Gibt den aktuell gedrueckten Button zurück oder den Wert 100 */
+uint8_t get_Button();
+#endif
